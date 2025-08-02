74187d83dee67d24a5894ea2fa8fd38c6b10631e
xingnian <j_xingnian@163.com>
2025-08-02 16:05:27 +0800

    创建项目

---
+++ main/main.c
@@ -0,0 +1,19 @@
+/*
+ * @Author: xingnian j_xingnian@163.com
+ * @Date: 2025-08-02 15:54:07
+ * @LastEditors: xingnian j_xingnian@163.com
+ * @LastEditTime: 2025-08-02 15:55:59
+ * @FilePath: \esp-brookesia-chunfeng\main\main.c
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
+ */
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+}
