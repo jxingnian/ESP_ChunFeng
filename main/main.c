75492d31c1683266ca76b67ad1d65bf0a452b072
xingnian <j_xingnian@163.com>
2025-08-28 16:43:18 +0800

    debug：显示

--- main/main.c
+++ main/main.c
@@ -2,21 +2,26 @@
  * @Author: xingnian j_xingnian@163.com
  * @Date: 2025-08-09 18:34:37
  * @LastEditors: xingnian j_xingnian@163.com
- * @LastEditTime: 2025-08-27 15:36:15
- * @FilePath: \esp-brookesia-chunfeng\main\main.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @LastEditTime: 2025-08-28 16:38:36
+ * @FilePath: \esp-chunfeng\main\main.c
+ * @Description: esp32春风-AI占卜助手
  */
 
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "nvs_flash.h"
 #include "wifi_manager.h"
 #include "audio_hal.h"
 #include "coze_chat_app.h"          // Coze 聊天组件核心头文件
+#include "ui.h"
+#include "I2C_Driver.h"
+#include "Display_SPD2010.h"
+#include "LVGL_Driver.h"
 
 extern float BAT_analogVolts;
 
-extern "C" void app_main()
+void app_main()
 {    
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -25,9 +30,14 @@ extern "C" void app_main()
     }
     ESP_ERROR_CHECK(ret);
     
+    I2C_Init();
+    LCD_Init();
+    LVGL_Init();    // 先初始化LVGL库
+    ui_init();      // 再初始化UI界面
     wifi_init_softap();     //WIFI
     while (1)
     {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(10));
+        lv_timer_handler();
     }
 }
